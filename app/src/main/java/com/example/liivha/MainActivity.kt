769c08da010682ed320f6f92062dada87616b5e7
Your Name <st10097679@rcconnect.com>
2024-08-21 10:53:51 +0200

    Adding Splash Screen With Annimation

--- app/src/main/java/com/example/liivha/MainActivity.kt
+++ app/src/main/java/com/example/liivha/MainActivity.kt
@@ -1,20 +1,30 @@
 package com.example.liivha
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.os.Handler
+import android.os.Looper
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        // Initialize the logo ImageView
+        val logoImageView: ImageView = findViewById(R.id.logoimageView)
+
+        // Load and start the animation
+        val logoAnimation = AnimationUtils.loadAnimation(this, R.anim.splashanimation)
+        logoImageView.startAnimation(logoAnimation)
+
+        // Delay for 3 seconds before moving to the MainActivity
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, GetStarted::class.java))
+            finish()
+        }, 3000) // 3-second delay
     }
 }
