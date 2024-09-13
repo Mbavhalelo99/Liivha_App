2899673f8197413e958501e734a8ec5e48af92c7
Your Name <st10097679@rcconnect.com>
2024-09-13 09:34:07 +0200

    FrontEnd for the Dashboard
    Signed-off-by: Your Name <st10097679@rcconnect.com>

--- app/src/main/java/com/example/liivha/Dashboard.kt
+++ app/src/main/java/com/example/liivha/Dashboard.kt
@@ -1,6 +1,8 @@
 package com.example.liivha
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,34 @@ class Dashboard : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        // Map button
+        val mapBtn = findViewById<ImageView>(R.id.mapBtn)
+        mapBtn.setOnClickListener {
+            val intent = Intent(this, MapView::class.java)
+            startActivity(intent)
+        }
+
+        // Settings button
+        val settingsBtn = findViewById<ImageView>(R.id.settingsBtn)
+        settingsBtn.setOnClickListener {
+            val intent = Intent(this, Settings::class.java)
+            startActivity(intent)
+        }
+
+        // Popular Place 1 button
+        val popLoc1Btn = findViewById<ImageView>(R.id.popLoc1Btn)
+        popLoc1Btn.setOnClickListener {
+            val intent = Intent(this, PopularPlaces::class.java)
+            startActivity(intent)
+        }
+
+        // Popular Place 2 button
+        val popLoc2Btn = findViewById<ImageView>(R.id.popLoc2Btn)
+        popLoc2Btn.setOnClickListener {
+            val intent = Intent(this, PopularPlaces::class.java)
+            startActivity(intent)
         }
     }
 }
